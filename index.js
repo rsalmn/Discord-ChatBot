ddf7adb53ea4bc69e512188244f39cdf403417b7
GarudaProjects <80512930+GarudaProjects@users.noreply.github.com>
2021-12-29 02:06:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fetch = require("node-fetch");
 
 const client = new Discord.Client();
-client.login("TOKEN");
+client.login(process.env.TOKEN);
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
   client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
